dbe042d6e026002fe714b913b41149f5b5e17d68
Hui Lin <hlin@catalyte.io>
2021-08-11 15:38:43 -0400

    Modified format

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './Footer.css';
 
-function Footer() {
-  return (
-    <div className="footer">
-      <hr />
-      <p className="row">&copy; 2021 Sports Apparel, Inc.</p>
-    </div>
-  );
-}
+const Footer = () => (
+  <div className="footer">
+    <hr />
+    <p className="row">&copy; 2021 Sports Apparel, Inc.</p>
+  </div>
+);
+
 export default Footer;
