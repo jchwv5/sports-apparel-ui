f2a89c57a90f2edf106eb8d9face4d1fb70d5e97
jhelton1985 <86621873+jhelton1985@users.noreply.github.com>
2021-08-11 15:58:54 -0600

    add code to initialize menu on products page

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,38 +1,26 @@
-import React, { Component } from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
 
-class Sidebar extends Component {
-  render() {
-    return () => (
-      <>
-        <nav>
-          <div className="nav-wrapper">
-            <a href="google.com" className="brand-logo">Logo</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a href="sass.html">Sass</a></li>
-              <li><a href="badges.html">Components</a></li>
-              <li><a href="collapsible.html">JavaScript</a></li>
-            </ul>
+const Sidebar = () => (
+  <>
+    <ul id="slide-out" className="sidenav">
+      <li>
+        <div className="user-view">
+          <div className="background">
+            <img alt="" src="images/office.jpg" />
           </div>
-        </nav>
-        <ul id="slide-out" className="sidenav">
-          <li>
-            <div className="user-view">
-              <div className="background">
-                <img alt="" src="images/office.jpg" />
-              </div>
-              <a href="#user"><img className="circle" alt="" src="images/yuna.jpg" /></a>
-              <a href="#name"><span className="white-text name">John Doe</span></a>
-              <a href="#email"><span className="white-text email">jdandturk@gmail.com</span></a>
-            </div>
-          </li>
-          <li><a href="#!">Second Link</a></li>
-          <li><div className="divider" /></li>
-          <li><a href="#subheader" className="subheader">Subheader</a></li>
-          <li><a href="waves" className="waves-effect">Third Link With Waves</a></li>
-        </ul>
-        <a href="google.com" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-      </>
-    );
-  }
-}
+          <a href="#user"><img className="circle" alt="" src="images/yuna.jpg" /></a>
+          <a href="#name"><span className="white-text name">John Doe</span></a>
+          <a href="#email"><span className="white-text email">jdandturk@gmail.com</span></a>
+        </div>
+      </li>
+      <li><a href="#!">Second Link</a></li>
+      <li><div className="divider" /></li>
+      <li><a href="#!" className="subheader">Subheader</a></li>
+      <li><a href="#!" className="waves-effect">Third Link With Waves</a></li>
+    </ul>
+    <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+  </>
+);
+
 export default Sidebar;
