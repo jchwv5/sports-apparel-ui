175fdc3fb239773dca168f0ddf937df95cacda4f
Michael Stonesifer <mstonesifer@catalyte.io>
2021-08-17 16:47:41 -0400

    build validate function

--- src/components/create-product-page/CreateProduct.js
+++ src/components/create-product-page/CreateProduct.js
@@ -4,22 +4,23 @@ import FormItem from '../form/FormItem';
 import FormItemDropdown from '../form/FormItemDropdown';
 import CreateProductService from './CreateProductService';
 import Constants from '../../utils/constants';
+// import validate from '../../utils/validate';
 
 const CreateProduct = () => {
   const [apiError, setApiError] = useState(false);
 
-  const [name, setName] = React.useState({ });
+  const [name, setName] = React.useState('');
   const onNameChange = (e) => {
     setName({ ...name, [e.target.name]: e.target.value });
   };
 
-  const [description, setDescription] = React.useState({ });
+  const [description, setDescription] = React.useState('');
   const onDescriptionChange = (e) => {
     setDescription({ ...description, [e.target.description]: e.target.value });
   };
 
   const demographics = ['Men', 'Women', 'Kids'];
-  const [demographic, setDemographic] = React.useState({ });
+  const [demographic, setDemographic] = React.useState('Men');
   const onDemographicChange = (e) => {
     setDemographic({ ...demographic, [e.target.demographic]: e.target.value });
   };
@@ -28,7 +29,7 @@ const CreateProduct = () => {
   useEffect(() => {
     CreateProductService.fetchCategories(setCategories, setApiError);
   }, []);
-  const [category, setCategory] = React.useState({ });
+  const [category, setCategory] = React.useState('');
   const onCategoryChange = (e) => {
     setCategory({ ...category, [e.target.category]: e.target.value });
   };
@@ -37,27 +38,27 @@ const CreateProduct = () => {
   useEffect(() => {
     CreateProductService.fetchTypes(setTypes, setApiError);
   }, []);
-  const [type, setType] = React.useState({ });
+  const [type, setType] = React.useState('');
   const onTypeChange = (e) => {
     setType({ ...type, [e.target.type]: e.target.value });
   };
 
-  const [brand, setBrand] = React.useState({ });
+  const [brand, setBrand] = React.useState('');
   const onBrandChange = (e) => {
     setBrand({ brand, [e.target.brand]: e.target.value });
   };
 
-  const [material, setMaterial] = React.useState({ });
+  const [material, setMaterial] = React.useState('');
   const onMaterialChange = (e) => {
     setMaterial({ material, [e.target.material]: e.target.value });
   };
 
-  const [price, setPrice] = React.useState({ });
+  const [price, setPrice] = React.useState('');
   const onPriceChange = (e) => {
     setPrice({ price, [e.target.price]: e.target.value });
   };
 
-  const [activeStatus, setActiveStatus] = React.useState({ });
+  const [activeStatus, setActiveStatus] = React.useState('Active');
   const onActiveStatusChange = (e) => {
     setActiveStatus({ price, [e.target.price]: e.target.value });
   };
@@ -65,7 +66,45 @@ const CreateProduct = () => {
   const productActive = ['Active', 'Inactive'];
 
   function validate() {
-    console.log('Ravi is best');
+    const valid = {
+      formIsValid: true,
+      message: ''
+    };
+    if (name === '') {
+      valid.message += 'Name cannot be empty\n';
+      // setName({ ...productData, name: '' });
+      valid.formIsValid = false;
+    }
+    if (!description) {
+      valid.message += 'Description cannot be empty\n';
+      // setProductData({ ...productData, description: '' });
+      valid.formIsValid = false;
+    }
+    if (!brand) {
+      valid.message += 'Brand cannot be empty\n';
+      // setProductData({ ...productData, brand: '' });
+      valid.formIsValid = false;
+    }
+    if (!material) {
+      valid.message += 'Material cannot be empty\n';
+      // setProductData({ ...productData, material: '' });
+      valid.formIsValid = false;
+    }
+    if (!price) {
+      valid.message += 'Price cannot be empty\n';
+      // setProductData({ ...productData, price: '' });
+      valid.formIsValid = false;
+    } else if (!(/^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$/.test(price))) {
+      valid.message += 'Price should be a positive number\n';
+      // setProductData({ ...productData, price: '' });
+      valid.formIsValid = false;
+    } else if (price.toString()[price.toString().length - 3] !== '.') {
+      valid.message += 'Price should be in dollars and cents\n';
+      // setProductData({ ...productData, price: '' });
+      valid.formIsValid = false;
+    }
+    console.log(name);
+    console.log(valid.message);
   }
 
   return (
