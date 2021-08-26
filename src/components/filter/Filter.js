672f89576ec2e4122d1cf1e107782dbba27471cf
jhelton1985 <86621873+jhelton1985@users.noreply.github.com>
2021-08-26 15:05:02 -0600

    add brands to brands dropdown

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import M from 'materialize-css/dist/js/materialize.min';
@@ -13,7 +14,48 @@ const SidebarFilter = () => {
           <div className="collapsible-header">
             Brand
           </div>
-          <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          <div className="collapsible-body">
+            <li>
+              <input type="checkbox" />
+              Adidas
+            </li>
+            <li>
+              <input type="checkbox" />
+              Champion
+            </li>
+            <li>
+              <input type="checkbox" />
+              Columbia Sportswear
+            </li>
+            <li>
+              <input type="checkbox" />
+              Jordan
+            </li>
+            <li>
+              <input type="checkbox" />
+              New Balance
+            </li>
+            <li>
+              <input type="checkbox" />
+              Nike
+            </li>
+            <li>
+              <input type="checkbox" />
+              Patagonia
+            </li>
+            <li>
+              <input type="checkbox" />
+              Puma
+            </li>
+            <li>
+              <input type="checkbox" />
+              Reebok
+            </li>
+            <li>
+              <input type="checkbox" />
+              Under Armour
+            </li>
+          </div>
         </li>
         <li>
           <div className="collapsible-header">
