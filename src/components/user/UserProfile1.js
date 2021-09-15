e0be61e087f42291361aafadd36d8aec62508de0
Andrew Bounitch <abounitch@catalyte.io>
2021-09-15 18:37:27 -0400

    add validation for user

--- src/components/user/UserProfile1.js
+++ src/components/user/UserProfile1.js
@@ -5,6 +5,7 @@ import UserProfileService from './UserProfileService';
 // import TextInputDropdown from '../form/TextInputDropdown';
 import style from './UserProfile.module.css';
 // import usStates from './usStates';
+import validate from '../../utils/validate';
 import Constants from '../../utils/constants';
 
 const UserProfile1 = () => {
@@ -12,7 +13,12 @@ const UserProfile1 = () => {
   const { email } = location.state;
   const [userInfo, setUserInfo] = React.useState([]);
   const [apiError, setApiError] = useState(false);
-
+  const [nameError, setNameError] = React.useState('');
+  const [descriptionError, setDescriptionError] = React.useState('');
+  const [demographicError, setDemographicError] = React.useState('');
+  const [categoryError, setCategoryError] = React.useState('');
+  const [typeError, setTypeError] = React.useState('');
+  const [materialError, setMaterialError] = React.useState('');
   const onUserChange = (e) => {
     setUserInfo((prevValue) => ({ ...prevValue, [e.target.id]: e.target.value }));
   };
@@ -20,7 +26,11 @@ const UserProfile1 = () => {
   useEffect(() => {
     UserProfileService.fetchUserInfo(setUserInfo, setApiError, email);
   }, [email]);
-
+  /**
+   *
+   * @name handleSubmit
+   * @description sends request to save valid form data, then displays status toast
+   */
   function handleSubmit() {
     UserProfileService.userUpdate(
       userInfo.id,
@@ -35,6 +45,42 @@ const UserProfile1 = () => {
       userInfo.phoneNumber,
       setApiError
     );
+  }
+  function validateForm() {
+    let formIsValid = true;
+    let result = validate('text', 'First Name', userInfo.firstName);
+    setNameError(result.errorMessage);
+    if (!result.dataIsValid) {
+      formIsValid = false;
+    }
+    result = validate('text', 'Last Name', userInfo.lastName);
+    setDescriptionError(result.errorMessage);
+    if (!result.dataIsValid) {
+      formIsValid = false;
+    }
+    result = validate('alphaNum', 'Street', userInfo.streetAddress);
+    setDemographicError(result.errorMessage);
+    if (!result.dataIsValid) {
+      formIsValid = false;
+    }
+    result = validate('text', 'City', userInfo.city);
+    setCategoryError(result.errorMessage);
+    if (!result.dataIsValid) {
+      formIsValid = false;
+    }
+    result = validate('text', 'State', userInfo.state);
+    setTypeError(result.errorMessage);
+    if (!result.dataIsValid) {
+      formIsValid = false;
+    }
+    result = validate('zip', 'Zip', userInfo.zipCode);
+    setMaterialError(result.errorMessage);
+    if (!result.dataIsValid) {
+      formIsValid = false;
+    }
+    if (formIsValid) {
+      handleSubmit();
+    }
   }
   return (
     <div>
@@ -51,6 +97,7 @@ const UserProfile1 = () => {
                 onChange={onUserChange}
                 value={userInfo.firstName}
               />
+              {nameError && <p className={style.errMsg}>{nameError}</p>}
               <TextInput
                 type="text"
                 id="lastName"
@@ -58,6 +105,7 @@ const UserProfile1 = () => {
                 onChange={onUserChange}
                 value={userInfo.lastName}
               />
+              {nameError && <p className={style.errMsg}>{nameError}</p>}
               <TextInput
                 placeholder="e.g. 123 Sesame Street"
                 type="text"
@@ -66,6 +114,7 @@ const UserProfile1 = () => {
                 onChange={onUserChange}
                 value={userInfo.streetAddress}
               />
+              {descriptionError && <p className={style.errMsg}>{descriptionError}</p>}
               <TextInput
                 placeholder="e.g. Unit #1"
                 type="text"
@@ -74,6 +123,7 @@ const UserProfile1 = () => {
                 onChange={onUserChange}
                 value={userInfo.streetAddress2}
               />
+              {demographicError && <p className={style.errMsg}>{demographicError}</p>}
               <TextInput
                 placeholder="e.g. Denver"
                 type="text"
@@ -82,14 +132,16 @@ const UserProfile1 = () => {
                 onChange={onUserChange}
                 value={userInfo.city}
               />
+              {categoryError && <p className={style.errMsg}>{categoryError}</p>}
               <TextInput
                 id="state"
-                label="state"
+                label="State"
                 color="secondary"
                 onChange={onUserChange}
                 placeholder="[Select State]"
                 value={userInfo.state}
               />
+              {typeError && <p className={style.errMsg}>{typeError}</p>}
               <TextInput
                 placeholder="e.g. 12345"
                 type="text"
@@ -98,16 +150,9 @@ const UserProfile1 = () => {
                 onChange={onUserChange}
                 value={userInfo.zipCode}
               />
-              <TextInput
-                placeholder="XXX-XXX-XXXX"
-                type="text"
-                id="phoneNumber"
-                label="Phone"
-                onChange={onUserChange}
-                value={userInfo.phoneNumber}
-              />
+              {materialError && <p className={style.errMsg}>{materialError}</p>}
             </div>
-            <button onClick={handleSubmit} type="button" className={style.saveButton1}>
+            <button onClick={validateForm} type="button" className={style.saveButton1}>
               Save changes
             </button>
             <button type="button" className={style.historyButton1}>
