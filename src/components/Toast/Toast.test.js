260444b9db863cc5f0203e9ff777f9526caf3348
Perry Nguyen <pnguyen@catalyte.io>
2021-09-17 12:06:54 -0700

    Adjust spacing, fixed some linting errors.

--- src/components/Toast/Toast.test.js
+++ src/components/Toast/Toast.test.js
@@ -2,15 +2,13 @@ import { toast } from 'react-toastify';
 import notify from './Toast';
 
 describe('notify', () => {
-    it("should have been called", () => {
-        expect(notify('success', 'it was a success')).toHaveBeenCalled;
-    })
-    
-})
+  it('should have been called', () => {
+    expect(notify('success', 'it was a success')).toHaveBeenCalled;
+  });
+});
 
 describe('notify', () => {
-    it("should be in the document", () => {
-        expect(notify('success', 'it was a success')).toBeInTheDocument;
-    })
-    
-})
+  it('should be in the document', () => {
+    expect(notify('success', 'it was a success')).toBeInTheDocument;
+  });
+});
